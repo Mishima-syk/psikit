764f2a57482553532d822c33b55f87a801863556
Kazufumi Ohkawa <kerolinq@gmail.com>
2018-10-02 15:55:16 +0900

    Add initial code

--- README.md
+++ README.md
@@ -1,35 +1,18 @@
 from [Calculate HOMO and LUMO with Psi4](https://iwatobipen.wordpress.com/2018/08/24/calculate-homo-and-lumo-with-psi4-rdkit-psi4/)
 
-# psikit as a wrapper library?
+# Usage
 
-    import psikit
-    import numpy as np
+    from psikit import Psikit
     from rdkit import Chem
+    from rdkit.Chem import AllChem
     
-    psikit.core.set_output_file("out.dat", True)
-    psikit.set_memory('4 GB')
-    psikit.set_num_threads(4)
+    pk = Psikit()
+    mol = Chem.MolFromSmiles("c1ccccc1")
+    pk.geometry(mol, addHs=True, optimize=True)
+    print("SCF Energy: ", pk.energy())
+    print("HOMO: ", pk.HOMO)
+    print("LUMO: ", pk.LUMO)
     
-    mol = Chem.MolFromSmiles("c1ccccc1")
-    benz = psikit.geometry(mol)
-    scf_e, scf_wfn = psikit.energy("B3LYP/cc-pVDZ", return_wfn=True)
-    HOMO = psikit.get_homo(scf_wfn)
-    LUMO = psikit.get_lumo(scf_wfn)
-    print(HOMO, LUMO, scf_e)
-
-# or psikit as a set of util functions library?
-
-    import psi4
-    from psikit import mol2xyz, get_homo, get_lumo
-    import numpy as np
-    from rdkit import Chem
-    psi4.core.set_output_file("out.dat", True)
-    mol = Chem.MolFromSmiles("c1ccccc1")
-    xyz, mol = mol2xyz(mol)
-    psi4.set_memory('4 GB')
-    psi4.set_num_threads(4)
-    benz = psi4.geometry(xyz)
-    scf_e, scf_wfn = psi4.energy("B3LYP/cc-pVDZ", return_wfn=True)
-    HOMO = get_homo(scf_wfn)
-    LUMO = get_lumo(scf_wfn)
-    print(HOMO, LUMO, scf_e)
+    # SCF Energy:  -232.26253075623467
+    # HOMO:  -0.2529008246007736
+    # LUMO:  -0.006506665366600545
---
+++ psikit/psikit.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+
+class Psikit(object):
+    def __init__(self, threads=2, memory=4):
+        import psi4
+        self.psi4 = psi4
+        self.psi4.core.set_output_file("psikit_out.dat", True)
+        self.psi4.set_memory("{} GB".format(memory))
+        self.psi4.set_num_threads(threads)
+        self.wfn = None
+
+    def geometry(self, mol, addHs=False, optimize=False):
+        if type(mol) == rdkit.Chem.rdchem.Mol:
+            if addHs:
+                mol = Chem.AddHs(mol)
+            if optimize:
+                AllChem.EmbedMolecule(mol, useExpTorsionAnglePrefs=True,useBasicKnowledge=True)
+                AllChem.UFFOptimizeMolecule(mol)
+            xyz = mol2xyz(mol)
+        self.psi4.geometry(xyz)        
+            
+    def energy(self, basis_sets= "B3LYP/cc-pVDZ", return_wfn=True):
+        scf_energy, wfn = self.psi4.energy(basis_sets, return_wfn=return_wfn)
+        self.wfn = wfn
+        return scf_energy
+    
+    @property
+    def HOMO(self):
+        return self.wfn.epsilon_a_subset('AO', 'ALL').np[self.wfn.nalpha()-1]
+
+    @property
+    def LUMO(self):
+        return self.wfn.epsilon_a_subset('AO', 'ALL').np[self.wfn.nalpha()]
+
+
+def mol2xyz(mol):
+    xyz_string = "\n"
+    for _, atom in enumerate(mol.GetAtoms()):
+        pos = mol.GetConformer().GetAtomPosition(atom.GetIdx())
+        xyz_string += "{} {} {} {}\n".format(atom.GetSymbol(), pos.x, pos.y, pos.z)
+    xyz_string += "units angstrom\n"
+    return xyz_string
