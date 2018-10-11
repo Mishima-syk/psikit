82f4d471b93d58ff1b88bf33789ebcb659c0e391
Kazufumi Ohkawa <kerolinq@gmail.com>
2018-10-11 11:31:42 +0900

    Fix packages option

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, find_packages
+from distutils.core import setup
 
 with open('README.md') as readme_file:
     long_description = readme_file.read()
@@ -12,7 +12,7 @@ setup(name='Psikit',
       author='Kazufumi Ohkawa, Takayuki Serizawa',
       author_email='kerolinq@gmail.com, ######',
       url='https://github.com/Mishima-syk/psikit',
-      packages=find_packages(exclude=('tests', 'docs')),
+      packages=['psikit'],
       install_requires=['rdkit','psi4',],
       license='MIT',
      )
