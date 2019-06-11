f88c95a60354f269c7f6e4613416f6cde4aace10
iwatobipen <seritaka@gmail.com>
2019-06-11 21:45:03 +0900

    moviz

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
     long_description = readme_file.read()
 
 setup(name='Psikit',
-      version='0.1.4',
+      version='0.1.5',
       description='A thin wrapper library for Psi4 and RDKit',
       long_description=long_description,
       long_description_content_type='text/markdown',
