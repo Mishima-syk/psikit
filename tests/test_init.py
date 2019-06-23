1efa2804c6971d33c0946cb4b8b1a0bf17dded47
Kazufumi Ohkawa <kerolinq@gmail.com>
2019-06-23 11:33:46 +0900

    Add test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,21 @@
 from psikit import Psikit
 import rdkit
+import pytest
 
-class TestPsikit(object):
-    def test_read_from_smiles(self):
-        pk = Psikit()
-        pk.read_from_smiles("C")
-        assert type(pk.mol) is rdkit.Chem.rdchem.Mol
+
+def test_read_from_smiles():
+    pk = Psikit()
+    pk.read_from_smiles("C")
+    assert type(pk.mol) is rdkit.Chem.rdchem.Mol
+
+def test_energy():
+    pk = Psikit()
+    pk.read_from_smiles("C")
+    energy = pk.energy()
+    assert pytest.approx(-40.1999631329161, 0.00000000000005) == energy
+
+def test_energy_sto3g():
+    pk = Psikit()
+    pk.read_from_smiles("C")
+    energy = pk.energy(basis_sets="scf/sto-3g")
+    assert pytest.approx(-39.72474793261219, 0.00000000000005) == energy
