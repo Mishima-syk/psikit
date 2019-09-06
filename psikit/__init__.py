164897b2697d09b58a3186f817d3fe0df2e38652
iwatobipen <seritaka@gmail.com>
2019-09-07 08:25:53 +0900

    separate SAPT from psikit.py

--- psikit/__init__.py
+++ psikit/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
 from psikit.psikit import Psikit
-from psikit.psikit import Sapt
--- tests/test_sapt.py
+++ tests/test_sapt.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from psikit import Psikit
-from psikit import Sapt
+from psikit.sapt import Sapt
 import rdkit
 import pytest
 
