f57911c85b814e570215f184619bb35dbc4ac719
iwatobipen <seritaka@gmail.com>
2020-11-07 21:15:13 +0900

    add conda recipe

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
     long_description = readme_file.read()
 
 setup(name='Psikit',
-      version='0.1.8',
+      version='0.2.0',
       description='A thin wrapper library for Psi4 and RDKit',
       long_description=long_description,
       long_description_content_type='text/markdown',
